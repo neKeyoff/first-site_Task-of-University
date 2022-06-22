f57fa6cd6691de0c6c1e91cecce09df1a5972972
Lozerd <dmisha29411@gmail.com>
2022-06-22 21:53:44 +0300

    added comments on right project structure

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+# Почитай про это
+# app_name = "main"
+
 urlpatterns = [
+    # path('', IndexView.as_view(), name="index"),  # Так определяются урлы для вьюх на классах
     path('', views.index),
     path('about-us', views.about),
     path('product', views.product),
--- main/views.py
+++ main/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = "main/index.html"
+
+    def get(self, request, *args, **kwargs):
+        """
+            Метод вызывается при загрузке страницы
+            Тут обычно инициализируют динамические переменные класса
+        """
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        """
+            Метод вызывается при отправке запроса с типом метода 'POST'
+         """
+        return self.get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        """
+            Здесь инициализируются любые переменные для вывода на шаблон
+            context - это словарь
+        """
+        context = super().get_context_data(**kwargs)
+        return context
 
 
 def index(request):
